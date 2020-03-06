e6462116bf3394ff4d84e3241c608f8279e2a0f0
yin <yin@swifthealth.cn>
2020-03-06 15:24:55 +0800

    ios

--- ios/RNNeteaseQuickpass.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-#import <NTESQuickPass/NTESQuickPass.h>
-
-@interface RNNeteaseQuickpass : RCTEventEmitter <RCTBridgeModule>
-
-@end
-  
