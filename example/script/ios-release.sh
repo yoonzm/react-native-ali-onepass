322e5f232bdb5843adc71bc31f3332f56f3eee09
尹智敏 <yinzhimin@yinzhimindeMBP.lan>
2020-03-09 23:36:23 +0800

    .

--- example/script/ios-release.sh
+++ example/script/ios-release.sh
@@ -2,16 +2,16 @@
 
 ./update_node_modules.sh
 
-cd ios
+cd ios || exit
 
 rm -rf build
 rm -rf DerivedData
 rm -rf ./IPADir/*
 
-today=`date +%Y-%m-%d`
+today=$(date +%Y-%m-%d)
 
 # ios自动化打包脚本
-project_path=`pwd`
+project_path=$(pwd)
 
 #工程名 将XXX替换成自己的工程名
 project_name=onepass
@@ -23,7 +23,7 @@ exportOptionsPlistPath=${project_path}/${project_name}/Info.plist
 
 # 参数1 ENV 参数2 VERSION_TYPE 参数3 NET_EASE_IM_KEY
 build(){
-    now=`date +%Y_%m_%d_%H_%M`
+    now=$(date +%Y_%m_%d_%H_%M)
 
     #压缩
     npm run ios-build
@@ -31,9 +31,9 @@ build(){
     archivePath=~/Library/Developer/Xcode/Archives/${today}/${project_name}-${now}.xcarchive
 
     xcodebuild \
-    archive -workspace ${project_path}/${project_name}.xcodeproj/project.xcworkspace \
+    archive -workspace "${project_path}"/${project_name}.xcodeproj/project.xcworkspace \
     -scheme ${project_name} \
-    -archivePath ${archivePath} || exit
+    -archivePath "${archivePath}" || exit
 
     #导出.ipa文件所在路径
 
@@ -41,9 +41,9 @@ build(){
 
     xcodebuild \
     -exportArchive \
-    -archivePath ${archivePath} \
-    -exportPath ${exportIpaPath} \
-    -exportOptionsPlist ${exportOptionsPlistPath} \
+    -archivePath "${archivePath}" \
+    -exportPath "${exportIpaPath}" \
+    -exportOptionsPlist "${exportOptionsPlistPath}" \
     || exit
 
     echo 打包结束
@@ -52,11 +52,11 @@ build(){
 build
 
 # 上传至蒲公英
-fileNames=`ls ./IPADir/`
+fileNames=$(ls ./IPADir/)
 for fileName in ${fileNames}
 do
     # 当前在android文件夹内
-    sh ../script/upload.sh uploadPGY ./IPADir/${fileName}/${project_name}.ipa ${fileName}
+    sh ../script/upload.sh uploadPGY ./IPADir/"${fileName}"/${project_name}.ipa "${fileName}"
 #    rm -rf ./IPADir/${fileName}
 done
 echo "已上传至蒲公英----------------------------------------------------------------------------------------------------"
